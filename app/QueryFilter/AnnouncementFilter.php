08b8a477cd928de2daf9150b906e935d9921266c
jamshid <jamshidqochqorov84@gmail.com>
2025-02-26 19:28:19 +0500

    fixed Announcement queryfilter

--- app/QueryFilter/AnnouncementFilter.php
+++ app/QueryFilter/AnnouncementFilter.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\QueryFilter;
 
 class AnnouncementFilter extends QueryFilter
@@ -8,6 +7,10 @@ class AnnouncementFilter extends QueryFilter
     public function title($value)
     {
         $this->builder->where('title', 'like', '%' . $value . '%');
+    }
+    public function description($value)
+    {
+        $this->builder->where('description', 'like', '%' . $value . '%');
     }
 
     public function category_id($value)
@@ -28,5 +31,25 @@ class AnnouncementFilter extends QueryFilter
     public function price_max($value)
     {
         $this->builder->where('price', '<=', $value);
+    }
+
+    public function attributes($values)
+    {
+        if (is_array($values)) {
+            foreach ($values as $key => $value) {
+                $this->builder->whereHas('attributes', function ($query) use ($key, $value) {
+                    $query->where('key', $key)->where('value', $value);
+                });
+            }
+        }
+    }
+
+    public function tags($values)
+    {
+        if (is_array($values)) {
+            $this->builder->whereHas('tags', function ($query) use ($values) {
+                $query->whereIn('tags.id', $values);
+            });
+        }
     }
 }
